8a031f166db91abe13354d9845e16b0743d32061
Tom Yuval <tomuvak@gmail.com>
2022-07-15 17:11:02 +0200

    Add KDoc comments

--- src/commonMain/kotlin/sequences/CachingSequence.kt
+++ src/commonMain/kotlin/sequences/CachingSequence.kt
@@ -1,5 +1,10 @@
 package com.tomuvak.util.sequences
 
+/**
+ * A sequence which yields the exact same elements as the given [original] sequence and is iterable multiple times –
+ * even if the original sequence is not. The original sequence is guaranteed not to be iterated over more than once,
+ * and, during the first (and only) iteration, not to be iterated over further than is necessary.
+ */
 class CachingSequence<T>(original: Sequence<T>): Sequence<T> {
     internal val iterator: Iterator<T> = original.iterator()
     private val cache: MutableList<T> = mutableListOf()
--- src/commonMain/kotlin/sequences/Sequences.kt
+++ src/commonMain/kotlin/sequences/Sequences.kt
@@ -1,5 +1,9 @@
 package com.tomuvak.util.sequences
 
+/**
+ * Returns (a sequence equivalent to) the receiver sequence [this] if it is not empty, or `null` if it is. Other than
+ * the initial check for emptiness, this operation is _intermediate_ and _stateless_.
+ */
 fun <T> Sequence<T>.ifNotEmpty(): Sequence<T>? {
     val iterator = iterator()
     return if (iterator.hasNext()) object : Sequence<T> {
@@ -14,7 +18,17 @@ fun <T> Sequence<T>.ifNotEmpty(): Sequence<T>? {
     } else null
 }
 
+/**
+ * Returns a sequence which yields the exact same elements as the receiver sequence [this] and is iterable multiple
+ * times – even if the original sequence is not. The original sequence is guaranteed not to be iterated over more than
+ * once, and, during the first (and only) iteration, not to be iterated over further than is necessary (that is this
+ * operation, while not _stateless_, is _intermediate_).
+ */
 fun <T> Sequence<T>.cached(): Sequence<T> = CachingSequence(this)
 
+/**
+ * Yields all elements of the receiver sequence [this] before the first one which is not of type [T] (if any; yields all
+ * elements if they're all of type T). This operation is _intermediate_ and _stateless_.
+ */
 @Suppress("UNCHECKED_CAST") inline fun <reified T> Sequence<*>.takeWhileIsInstance(): Sequence<T> =
     takeWhile { it is T } as Sequence<T>
