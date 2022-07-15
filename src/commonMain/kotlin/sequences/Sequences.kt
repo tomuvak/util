23725f1a4d1bd6045d41363114087b9c7042f855
Tom Yuval <tomuvak@gmail.com>
2022-07-15 23:22:54 +0200

    Sequences: add functions singleOrNullIf*

--- src/commonMain/kotlin/sequences/Sequences.kt
+++ src/commonMain/kotlin/sequences/Sequences.kt
@@ -1,6 +1,108 @@
 package com.tomuvak.util.sequences
 
 /**
+ * Returns the single element in the receiver sequence [this], or `null` if the sequence is empty.
+ *
+ * Throws if the sequence contains more than one element.
+ *
+ * This operation is _terminal_.
+ *
+ * This is different from the standard library's [singleOrNull] in that this function throws (rather than return `null`)
+ * in case the sequence contains more than one element.
+ */
+fun <T> Sequence<T>.singleOrNullIfEmpty(): T? {
+    val iterator = iterator()
+    if (!iterator.hasNext()) return null
+    val ret = iterator.next()
+    return if (iterator.hasNext()) throw IllegalArgumentException("Sequence has more than one element.") else ret
+}
+
+/**
+ * Returns the single element in the receiver sequence [this] which satisfies the given [predicate], or `null` if the
+ * sequence contains no element satisfying the predicate.
+ *
+ * Throws if the sequence contains more than one element satisfying the predicate.
+ *
+ * This operation is _terminal_.
+ *
+ * This is different from the standard library's [singleOrNull] in that this function throws (rather than return `null`)
+ * in case the sequence contains more than one element satisfying the predicate.
+ */
+fun <T> Sequence<T>.singleOrNullIfNone(predicate: (T) -> Boolean): T? {
+    var ret: T? = null
+    var found = false
+    for (element in this)
+        if (predicate(element)) {
+            if (found) throw IllegalArgumentException("Sequence contains more than one matching element.")
+            found = true
+            ret = element
+        }
+    return ret
+}
+
+/**
+ * Returns the single element in the receiver sequence [this], or `null` if the sequence contains multiple elements.
+ *
+ * Throws if the sequence is empty.
+ *
+ * This operation is _terminal_.
+ *
+ * This is different from the standard library's [singleOrNull] in that this function throws (rather than return `null`)
+ * in case the sequence is empty.
+ */
+fun <T> Sequence<T>.singleOrNullIfMultiple(): T? {
+    val iterator = iterator()
+    if (!iterator.hasNext()) throw NoSuchElementException("Sequence is empty.")
+    val ret = iterator.next()
+    return if (iterator.hasNext()) null else ret
+}
+
+/**
+ * Returns the single element in the receiver sequence [this] which satisfies the given [predicate], or `null` if the
+ * sequence contains multiple elements satisfying the predicate.
+ *
+ * Throws if the sequence contains no element satisfying the predicate.
+ *
+ * This operation is _terminal_.
+ *
+ * This is different from the standard library's [singleOrNull] in that this function throws (rather than return `null`)
+ * in case the sequence contains no element satisfying the predicate.
+ */
+fun <T> Sequence<T>.singleOrNullIfMultiple(predicate: (T) -> Boolean): T? {
+    var ret: T? = null
+    var found = false
+    for (element in this)
+        if (predicate(element)) {
+            if (found) return null
+            found = true
+            ret = element
+        }
+    return if (found) ret else throw NoSuchElementException("Sequence contains no element matching the predicate.")
+}
+
+/**
+ * Returns the single element in the receiver sequence [this], or `null` if the sequence is empty or contains multiple
+ * elements.
+ *
+ * This operation is _terminal_.
+ *
+ * This is equivalent to the standard library's [singleOrNull], but has a more explicit name that makes the semantics
+ * clearer.
+ */
+fun <T> Sequence<T>.singleOrNullIfEmptyOrMultiple(): T? = singleOrNull()
+
+/**
+ * Returns the single element in the receiver sequence [this] which satisfies the given [predicate], or `null` if the
+ * sequence contains either no element satisfying the predicate or multiple such elements.
+ *
+ * This operation is _terminal_.
+ *
+ * This is equivalent to the standard library's [singleOrNull], but has a more explicit name that makes the semantics
+ * clearer.
+ */
+fun <T> Sequence<T>.singleOrNullIfNoneOrMultiple(predicate: (T) -> Boolean): T? = singleOrNull(predicate)
+
+/**
  * Returns the single element in the receiver sequence [this], or throws if the sequence is empty. Not supposed to be
  * called on a sequence with more than one element – but this function makes no effort to verify that that's not the
  * case.
--- src/commonTest/kotlin/sequences/SequencesTest.kt
+++ src/commonTest/kotlin/sequences/SequencesTest.kt
@@ -6,6 +6,143 @@ import com.tomuvak.testing.assertions.testIntermediateOperation
 import kotlin.test.*
 
 class SequencesTest {
+    @Test fun singleOrNullIfEmptyReturnsNullWhenEmpty() =
+        assertNull(emptySequence<Int>().constrainOnce().singleOrNullIfEmpty())
+    @Test fun singleOrNullIfEmptyReturnsValueWhenSingle() =
+        assertEquals(3, sequenceOf(3).constrainOnce().singleOrNullIfEmpty())
+    @Test fun singleOrNullIfEmptyThrowsWhenMultiple() {
+        assertFailsWith<IllegalArgumentException> { sequenceOf(3, 3).constrainOnce().singleOrNullIfEmpty() }
+    }
+    @Test fun singleOrNullIfEmptyDoesNotEvaluateFurther() {
+        assertFailsWith<IllegalArgumentException> { sequence {
+            yield(3)
+            yield(3)
+            fail("Not supposed to enumerate thus far")
+        }.constrainOnce().singleOrNullIfEmpty() }
+    }
+
+    @Test fun singleOrNullIfNoneReturnsNullWhenEmpty() =
+        assertNull(emptySequence<Int>().constrainOnce().singleOrNullIfNone { fail("Not supposed to be called") })
+    @Test fun singleOrNullIfNoneReturnsNullWhenNone() {
+        val testedElements = mutableListOf<Int>()
+        assertNull(sequenceOf(1, 2, 3).constrainOnce().singleOrNullIfNone {
+            testedElements.add(it)
+            false
+        })
+        assertEquals(listOf(1, 2, 3), testedElements)
+    }
+    @Test fun singleOrNullIfNoneReturnsValueWhenSingle() {
+        val testedElements = mutableListOf<Int>()
+        assertEquals(
+            3,
+            sequenceOf(1, 2, 3, 4, 5).constrainOnce().singleOrNullIfNone {
+                testedElements.add(it)
+                it == 3
+            }
+        )
+        assertEquals(listOf(1, 2, 3, 4, 5), testedElements)
+    }
+    @Test fun singleOrNullIfNoneThrowsWhenMultiple() {
+        assertFailsWith<IllegalArgumentException> {
+            sequence {
+                yield(1)
+                yield(2)
+                yield(3)
+                yield(3)
+                fail("Not supposed to enumerate thus far")
+            }.constrainOnce().singleOrNullIfNone { it == 3 }
+        }
+    }
+
+    @Test fun singleOrNullIfMultipleThrowsWhenEmpty() {
+        assertFailsWith<NoSuchElementException> { emptySequence<Int>().constrainOnce().singleOrNullIfMultiple() }
+    }
+    @Test fun singleOrNullIfMultipleReturnsValueWhenSingle() =
+        assertEquals(3, sequenceOf(3).constrainOnce().singleOrNullIfMultiple())
+    @Test fun singleOrNullIfMultipleReturnsNullWhenMultiple() =
+        assertNull(sequenceOf(3, 3).constrainOnce().singleOrNullIfMultiple())
+    @Test fun singleOrNullIfMultipleDoesNotEvaluateFurther() = assertNull(sequence {
+        yield(3)
+        yield(3)
+        fail("Not supposed to enumerate thus far")
+    }.constrainOnce().singleOrNullIfMultiple())
+
+    @Test fun singleOrNullIfMultipleWithPredicateThrowsWhenEmpty() {
+        assertFailsWith<NoSuchElementException> {
+            emptySequence<Int>().constrainOnce().singleOrNullIfMultiple { fail("Not supposed to be called") }
+        }
+    }
+    @Test fun singleOrNullIfMultipleWithPredicateThrowsWhenNone() {
+        val testedElements = mutableListOf<Int>()
+        assertFailsWith<NoSuchElementException> {
+            sequenceOf(1, 2, 3).constrainOnce().singleOrNullIfMultiple {
+                testedElements.add(it)
+                false
+            }
+        }
+        assertEquals(listOf(1, 2, 3), testedElements)
+    }
+    @Test fun singleOrNullIfMultipleWithPredicateReturnsValueWhenSingle() {
+        val testedElements = mutableListOf<Int>()
+        assertEquals(
+            3,
+            sequenceOf(1, 2, 3, 4, 5).constrainOnce().singleOrNullIfMultiple {
+                testedElements.add(it)
+                it == 3
+            }
+        )
+        assertEquals(listOf(1, 2, 3, 4, 5), testedElements)
+    }
+    @Test fun singleOrNullIfMultipleWithPredicateReturnsNullWhenMultiple() = assertNull(sequence {
+        yield(1)
+        yield(2)
+        yield(3)
+        yield(3)
+        fail("Not supposed to enumerate thus far")
+    }.constrainOnce().singleOrNullIfMultiple { it == 3 })
+
+    @Test fun singleOrNullIfEmptyOrMultipleReturnsNullWhenEmpty() =
+        assertNull(emptySequence<Int>().constrainOnce().singleOrNullIfEmptyOrMultiple())
+    @Test fun singleOrNullIfEmptyOrMultipleReturnsValueWhenSingle() =
+        assertEquals(3, sequenceOf(3).constrainOnce().singleOrNullIfEmptyOrMultiple())
+    @Test fun singleOrNullIfEmptyOrMultipleReturnsNullWhenMultiple() =
+        assertNull(sequenceOf(3, 3).constrainOnce().singleOrNullIfEmptyOrMultiple())
+    @Test fun singleOrNullIfEmptyOrMultipleDoesNotEvaluateFurther() = assertNull(sequence {
+        yield(3)
+        yield(3)
+        fail("Not supposed to enumerate thus far")
+    }.constrainOnce().singleOrNullIfEmptyOrMultiple())
+
+    @Test fun singleOrNullIfNoneOrMultipleReturnsNullWhenEmpty() = assertNull(
+        emptySequence<Int>().constrainOnce().singleOrNullIfNoneOrMultiple { fail("Not supposed to be called") }
+    )
+    @Test fun singleOrNullIfNoneOrMultipleReturnsNullWhenNone() {
+        val testedElements = mutableListOf<Int>()
+        assertNull(sequenceOf(1, 2, 3).constrainOnce().singleOrNullIfNoneOrMultiple {
+            testedElements.add(it)
+            false
+        })
+        assertEquals(listOf(1, 2, 3), testedElements)
+    }
+    @Test fun singleOrNullIfNoneOrMultipleReturnsValueWhenSingle() {
+        val testedElements = mutableListOf<Int>()
+        assertEquals(
+            3,
+            sequenceOf(1, 2, 3, 4, 5).constrainOnce().singleOrNullIfNoneOrMultiple {
+                testedElements.add(it)
+                it == 3
+            }
+        )
+        assertEquals(listOf(1, 2, 3, 4, 5), testedElements)
+    }
+    @Test fun singleOrNullIfNoneOrMultipleReturnsNullWhenMultiple() = assertNull(sequence {
+        yield(1)
+        yield(2)
+        yield(3)
+        yield(3)
+        fail("Not supposed to enumerate thus far")
+    }.constrainOnce().singleOrNullIfNoneOrMultiple { it == 3 })
+
     @Test fun singleNoVerifyThrowsWhenEmpty() {
         assertFailsWith<NoSuchElementException> { emptySequence<Int>().constrainOnce().singleNoVerify() }
     }
