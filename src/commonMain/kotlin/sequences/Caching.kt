72ed5f539c6d33b596f0b57a718343a41ac1f7f0
Tom Yuval <tomuvak@gmail.com>
2022-07-19 17:27:26 +0200

    sequences/Caching: add overload Sequence<>.cached(maxIterations)

--- src/commonMain/kotlin/sequences/Caching.kt
+++ src/commonMain/kotlin/sequences/Caching.kt
@@ -16,6 +16,27 @@ package com.tomuvak.util.sequences
  */
 fun <T> Sequence<T>.cached(): Sequence<T> = if (this is CachingSequence<T>) this else CachingSequence(this)
 
+/**
+ * Returns a sequence which yields the exact same elements as the receiver sequence [this] and is iterable up to
+ * [maxIterations] (which has to be positive) times – potentially in parallel – even if the original sequence is not.
+ * The original sequence is guaranteed not to be iterated over more than once, and, at any point during the first (and
+ * only) iteration, not to be iterated over further than has been required by the most advanced of the (at most
+ * [maxIterations]) iterations of the returned sequence.
+ *
+ * The behavior of the returned sequence in case iterating the original sequence throws an error at any point is
+ * undefined.
+ *
+ * Each of the elements of the original sequence is accessible as long as it has not been iterated over [maxIterations]
+ * times through the returned sequence. To allow them to be garbage-collected, make sure to iterate the returned
+ * sequence completely [maxIterations] times, or otherwise not to keep references to it once it's no longer needed.
+ *
+ * This operation is _intermediate_ and _stateful_.
+ */
+fun <T> Sequence<T>.cached(maxIterations: Int): Sequence<T> {
+    require(maxIterations > 0) { "maxIterations must be positive (was $maxIterations)" }
+    return ConstrainedCachingSequence(this, maxIterations)
+}
+
 private class CachingSequence<T>(original: Sequence<T>): Sequence<T> {
     private val iterator: Iterator<T> by lazy { original.iterator() }
     private val cache: MutableList<T> = mutableListOf()
@@ -27,4 +48,31 @@ private class CachingSequence<T>(original: Sequence<T>): Sequence<T> {
     }
 
     override fun iterator(): Iterator<T> = CachingSequenceIterator()
+}
+
+internal class ConstrainedCachingSequence<T>(original: Sequence<T>, private val maxIterations: Int): Sequence<T> {
+    private val iterator: Iterator<T> by lazy { original.iterator() }
+    private var numBygoneElements = 0
+    internal val cache: MutableList<T> = mutableListOf()
+    private var numYieldedIterators: Int = 0
+    private var iterators = List(maxIterations) { CachingSequenceIterator() }
+
+    private inner class CachingSequenceIterator: Iterator<T> {
+        private var index = 0
+        override fun hasNext(): Boolean = index < numBygoneElements + cache.size || iterator.hasNext()
+        override fun next(): T {
+            val actualIndex = index++ - numBygoneElements
+            if (actualIndex >= cache.size) cache.add(iterator.next())
+            val ret = cache[actualIndex]
+            if (actualIndex == 0 && iterators.all { it.index >= index }) {
+                cache.removeAt(0)
+                numBygoneElements++
+            }
+            return ret
+        }
+    }
+
+    override fun iterator(): Iterator<T> =
+        if (numYieldedIterators < maxIterations) iterators[numYieldedIterators++]
+        else throw IllegalStateException("Constrained cached sequence can only be iterated $maxIterations times")
 }
--- src/commonTest/kotlin/sequences/CachingTest.kt
+++ src/commonTest/kotlin/sequences/CachingTest.kt
@@ -1,8 +1,10 @@
 package com.tomuvak.util.sequences
 
+import com.tomuvak.testing.assertions.assertFailsWithTypeAndMessageContaining
 import com.tomuvak.testing.assertions.assertStartsWith
 import com.tomuvak.testing.assertions.assertValues
 import com.tomuvak.testing.assertions.mootProvider
+import com.tomuvak.util.nextOrNull
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertSame
@@ -14,23 +16,19 @@ class CachingTest {
         sequenceOf(1, 2).cached().assertValues(1, 2)
         sequenceOf(1, 2, 3).cached().assertValues(1, 2, 3)
     }
-
     @Test fun cachedSequenceYieldsSameElementsOnReiteration() {
-        emptySequence<Int>().cached().assertReiterableWithSameElements()
-        sequenceOf(1).cached().assertReiterableWithSameElements()
-        sequenceOf(1, 2).cached().assertReiterableWithSameElements()
-        sequenceOf(1, 2, 3).cached().assertReiterableWithSameElements()
+        emptySequence<Int>().cached().assertReiterableWithSameElements(4)
+        sequenceOf(1).cached().assertReiterableWithSameElements(4)
+        sequenceOf(1, 2).cached().assertReiterableWithSameElements(4)
+        sequenceOf(1, 2, 3).cached().assertReiterableWithSameElements(4)
     }
-
     @Test fun cachedSequenceDoesNotReiterateOriginalSequence() {
-        emptySequence<Int>().constrainOnce().cached().assertReiterableWithSameElements()
-        sequenceOf(1).constrainOnce().cached().assertReiterableWithSameElements()
-        sequenceOf(1, 2).constrainOnce().cached().assertReiterableWithSameElements()
-        sequenceOf(1, 2, 3).constrainOnce().cached().assertReiterableWithSameElements()
+        emptySequence<Int>().constrainOnce().cached().assertReiterableWithSameElements(4)
+        sequenceOf(1).constrainOnce().cached().assertReiterableWithSameElements(4)
+        sequenceOf(1, 2).constrainOnce().cached().assertReiterableWithSameElements(4)
+        sequenceOf(1, 2, 3).constrainOnce().cached().assertReiterableWithSameElements(4)
     }
-
     @Test fun cachedSequenceDoesNotTryToIterateOriginalSequenceBeforeItHasTo() { Sequence<Int>(mootProvider).cached() }
-
     @Test fun cachedSequenceEnumeratesOriginalSequenceLazily() {
         var lastEnumerated = -1
         val cachedSequence = sequenceOf(0, 1, 2).onEach { lastEnumerated = it }.cached()
@@ -43,12 +41,119 @@ class CachingTest {
         cachedSequence.assertStartsWith(0, 1, 2)
         assertEquals(2, lastEnumerated)
     }
-
     @Test fun cachedSequenceIsOriginalSequenceWhenAlreadyCached() {
         val sequence = Sequence<Int>(mootProvider).cached()
         assertSame(sequence, sequence.cached())
     }
 
-    private inline fun <reified T> Sequence<T>.assertReiterableWithSameElements() =
-        assertValues(*toList().toTypedArray())
+    @Test fun cachedRequiresPositiveArgument() {
+        for (i in -3..0)
+            assertFailsWithTypeAndMessageContaining<IllegalArgumentException>("positive", i) {
+                Sequence<Int>(mootProvider).cached(i)
+            }
+    }
+    @Test fun constrainedCachedSequenceHasSameElements() {
+        emptySequence<Int>().cached(3).assertValues()
+        sequenceOf(1).cached(3).assertValues(1)
+        sequenceOf(1, 2).cached(3).assertValues(1, 2)
+        sequenceOf(1, 2, 3).cached(3).assertValues(1, 2, 3)
+    }
+    @Test fun constrainedCachedSequenceYieldsSameElementsOnReiteration() {
+        emptySequence<Int>().cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1).cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1, 2).cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1, 2, 3).cached(3).assertReiterableWithSameElements(3)
+    }
+    @Test fun constrainedCachedSequenceCannotBeIteratedMoreThanMaxIterations() {
+        val cached = sequenceOf(1, 2, 3).cached(5)
+        repeat(5) { cached.iterator() }
+        assertFailsWithTypeAndMessageContaining<IllegalStateException>(5) { cached.iterator() }
+    }
+    @Test fun constrainedCachedSequenceDoesNotReiterateOriginalSequence() {
+        emptySequence<Int>().constrainOnce().cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1).constrainOnce().cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1, 2).constrainOnce().cached(3).assertReiterableWithSameElements(3)
+        sequenceOf(1, 2, 3).constrainOnce().cached(3).assertReiterableWithSameElements(3)
+    }
+    @Test fun constrainedCachedSequenceDoesNotTryToIterateOriginalSequenceBeforeItHasTo() {
+        Sequence<Int>(mootProvider).cached(3)
+    }
+    @Test fun constrainedCachedSequenceEnumeratesOriginalSequenceLazily() {
+        var lastEnumerated = -1
+        val cached = sequenceOf(0, 1, 2).onEach { lastEnumerated = it }.cached(4)
+        cached.assertStartsWith()
+        assertEquals(-1, lastEnumerated)
+        cached.assertStartsWith(0)
+        assertEquals(0, lastEnumerated)
+        cached.assertStartsWith(0, 1)
+        assertEquals(1, lastEnumerated)
+        cached.assertStartsWith(0, 1, 2)
+        assertEquals(2, lastEnumerated)
+    }
+    @Test fun constrainedCachedSequenceForgetsElementsWhenNoLongerNeeded() {
+        val cached = sequenceOf(0, 1, 2, 3).cached(6) as ConstrainedCachingSequence<Int>
+        assertEquals(0, cached.cache.size)
+        cached.assertReiterableWithSameElements(5)
+        assertEquals(4, cached.cache.size)
+
+        val iterator = cached.iterator()
+        assertEquals(0, iterator.next())
+        assertEquals(3, cached.cache.size)
+        assertEquals(1, iterator.next())
+        assertEquals(2, cached.cache.size)
+        assertEquals(2, iterator.next())
+        assertEquals(1, cached.cache.size)
+        assertEquals(3, iterator.next())
+        assertEquals(0, cached.cache.size)
+    }
+    @Test fun constrainedCachedSequenceBehavesCorrectlyDuringParallelIteration() {
+        var lastEnumerated = -1
+        val cached =
+            sequenceOf(0, 1, 2, 3, 4).onEach { lastEnumerated = it }.cached(4) as ConstrainedCachingSequence<Int>
+        val iterators = List(4) { cached.iterator() }
+
+        assertEquals(-1, lastEnumerated)
+        assertEquals(0, cached.cache.size)
+
+        fun verify(iteratorIndex: Int, expectedNextValue: Int?, expectedLastEnumerated: Int, expectedCacheSize: Int) {
+            assertEquals(expectedNextValue, iterators[iteratorIndex].nextOrNull())
+            assertEquals(expectedLastEnumerated, lastEnumerated)
+            assertEquals(cached.cache.size, expectedCacheSize)
+        }
+
+        verify(0, 0, 0, 1)
+        verify(1, 0, 0, 1)
+        verify(2, 0, 0, 1)
+
+        verify(2, 1, 1, 2)
+        verify(3, 0, 1, 1)
+        verify(0, 1, 1, 1)
+
+        verify(0, 2, 2, 2)
+
+        verify(0, 3, 3, 3)
+        verify(1, 1, 3, 3)
+        verify(1, 2, 3, 3)
+        verify(2, 2, 3, 3)
+        verify(2, 3, 3, 3)
+
+        verify(2, 4, 4, 4)
+        verify(3, 1, 4, 3)
+        verify(3, 2, 4, 2)
+        verify(3, 3, 4, 2)
+        verify(3, 4, 4, 2)
+
+        verify(3, null, 4, 2)
+        verify(0, 4, 4, 2)
+        verify(1, 3, 4, 1)
+        verify(2, null, 4, 1)
+        verify(0, null, 4, 1)
+        verify(1, 4, 4, 0)
+        verify(1, null, 4, 0)
+    }
+
+    private inline fun <reified T> Sequence<T>.assertReiterableWithSameElements(numIterations: Int) {
+        val valuesFromFirstIteration = toList().toTypedArray()
+        repeat(numIterations - 1) { assertValues(*valuesFromFirstIteration) }
+    }
 }
