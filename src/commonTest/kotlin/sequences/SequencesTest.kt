215e1ea47e89d1e5641d98df7029c9399fc64fdc
Tom Yuval <tomuvak@gmail.com>
2022-07-19 07:46:28 +0200

    SequencesTest (style): remove blank line between tests for same function (fix-up to 1117432c596d6d7815515bef52efbf279bf71bc6)

--- src/commonTest/kotlin/sequences/SequencesTest.kt
+++ src/commonTest/kotlin/sequences/SequencesTest.kt
@@ -140,7 +140,6 @@ class SequencesTest {
         sequenceOf<Number>(1L, 2).testIntermediateOperation({ takeWhileIsInstance<Int>() }) { assertValues() }
         sequenceOf<Number>(1L, 2L).testIntermediateOperation({ takeWhileIsInstance<Int>() }) { assertValues() }
     }
-
     @Test fun takeWhileIsInstanceEnumeratesSequenceLazily() {
         emptySequence<Number>().testLazyIntermediateOperation({ takeWhileIsInstance<Int>() }) { assertStartsWith() }
         sequenceOf<Number>(1).testLazyIntermediateOperation({ takeWhileIsInstance<Int>() }) { assertStartsWith(1) }
