45856c336294a40586ef710cdeda742313aebe0e
Tom Yuval <tomuvak@gmail.com>
2022-07-26 01:35:22 +0200

    Add Tuples with (functorial and) monadic operations for Pairs and Triples (with same-type components)

---
+++ src/commonMain/kotlin/Tuples.kt
@@ -0,0 +1,11 @@
+package com.tomuvak.util
+
+fun <T, R> Pair<T, T>.map(transform: (T) -> R): Pair<R, R> = Pair(transform(first), transform(second))
+fun <T, R> Pair<T, T>.flatMap(transform: (T) -> Pair<R, R>): Pair<R, R> = map(transform).flatten()
+fun <T> Pair<Pair<T, T>, Pair<T, T>>.flatten(): Pair<T, T> = Pair(first.first, second.second)
+
+fun <T, R> Triple<T, T, T>.map(transform: (T) -> R): Triple<R, R, R> =
+    Triple(transform(first), transform(second), transform(third))
+fun <T, R> Triple<T, T, T>.flatMap(transform: (T) -> Triple<R, R, R>): Triple<R, R, R> = map(transform).flatten()
+fun <T> Triple<Triple<T, T, T>, Triple<T, T, T>, Triple<T, T, T>>.flatten(): Triple<T, T, T> =
+    Triple(first.first, second.second, third.third)
---
+++ src/commonTest/kotlin/TuplesTest.kt
@@ -0,0 +1,31 @@
+package com.tomuvak.util
+
+import com.tomuvak.testing.assertions.scriptedFunction
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TuplesTest {
+    @Test fun mapsPair() = assertEquals(Pair("one", "two"), Pair(1, 2).map(scriptedFunction(1 to "one", 2 to "two")))
+    @Test fun flatMapsPair() = assertEquals(
+        Pair("one", "four"),
+        Pair(1, 2).flatMap(scriptedFunction(1 to Pair("one", "two"), 2 to Pair("three", "four")))
+    )
+    @Test fun flattensPair() = assertEquals(Pair(1, 4), Pair(Pair(1, 2), Pair(3, 4)).flatten())
+
+    @Test fun mapsTriple() = assertEquals(
+        Triple("one", "two", "three"),
+        Triple(1, 2, 3).map(scriptedFunction(1 to "one", 2 to "two", 3 to "three"))
+    )
+    @Test fun flatMapsTriple() = assertEquals(
+        Triple("one", "five", "nine"),
+        Triple(1, 2, 3).flatMap(scriptedFunction(
+            1 to Triple("one", "two", "three"),
+            2 to Triple("four", "five", "six"),
+            3 to Triple("seven", "eight", "nine")
+        ))
+    )
+    @Test fun flattensTriple() = assertEquals(
+        Triple(1, 5, 9),
+        Triple(Triple(1, 2, 3), Triple(4, 5, 6), Triple(7, 8, 9)).flatten()
+    )
+}
