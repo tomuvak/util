da83ec7aeab6b331fd442e47ea14f3766ddf35a5
Tom Yuval <tomuvak@gmail.com>
2022-08-01 20:10:39 +0200

    sequences/Sequences (style): break line differently in Sequence<>.takeWhileIsInstance and use <Any?> rather than <*>

--- src/commonMain/kotlin/sequences/Sequences.kt
+++ src/commonMain/kotlin/sequences/Sequences.kt
@@ -22,5 +22,5 @@ fun <T> Sequence<T>.ifNotEmpty(): Sequence<T>? {
  * Yields all elements of the receiver sequence [this] before the first one which is not of type [T] (if any; yields all
  * elements if they're all of type T). This operation is _intermediate_ and _stateless_.
  */
-@Suppress("UNCHECKED_CAST") inline fun <reified T> Sequence<*>.takeWhileIsInstance(): Sequence<T> =
-    takeWhile { it is T } as Sequence<T>
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Sequence<Any?>.takeWhileIsInstance(): Sequence<T> = takeWhile { it is T } as Sequence<T>
