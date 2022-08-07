da1c36d141d641abdac8e3a0d195f37cfa0f5f72
Tom Yuval <tomuvak@gmail.com>
2022-08-07 13:18:34 +0200

    Tests (refactor): add dependency on com.tomuvak.weak-reference to avoid (wrongful) "Cannot access class" error message on IntelliJ IDEA

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,7 @@ kotlin {
                 implementation("com.tomuvak.testing-assertions:testing-assertions:0.0.4")
                 implementation("com.tomuvak.testing-coroutines:testing-coroutines:0.0.1")
                 implementation("com.tomuvak.testing-gc:testing-gc:0.0.3")
+                implementation("com.tomuvak.weak-reference:weak-reference:0.0.2")
             }
         }
         val jvmMain by getting
