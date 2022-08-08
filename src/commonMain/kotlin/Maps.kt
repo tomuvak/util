5a5414acc6fda81d2cfde9d7c1318eba49213802
Tom Yuval <tomuvak@gmail.com>
2022-08-08 23:10:27 +0200

    Maps: add extension function MutableMap<,>.putIfAbsentAndGet(key, default/defaultProvider) (three overloads)

--- src/commonMain/kotlin/Maps.kt
+++ src/commonMain/kotlin/Maps.kt
@@ -4,6 +4,9 @@ package com.tomuvak.util
  * Returns the value associated with the given [key] in the receiver map [this], or the given [default] if the key is
  * not in the map.
  *
+ * Similar to [putIfAbsentAndGet] (for a mutable map), but that function also associates the given key with the given
+ * default value in the map in case the map does not already contain the given key.
+ *
  * Similar to the standard library's [getOrElse], but returns the value associated with the given [key] – rather than
  * the given [default] – also when that value is `null`. Also, this overload takes the default value itself, rather than
  * a function which computes it; there's another overload which takes such a function, as well as an overload which
@@ -21,6 +24,9 @@ inline fun <K, reified V> Map<K, V>.getOr(key: K, default: V): V = getOr(key) { 
 /**
  * Returns the value associated with the given [key] in the receiver map [this], or, if the key is not in the map,
  * invokes the given [defaultProvider] and returns the result.
+ *
+ * Similar to [putIfAbsentAndGet] (for a mutable map), but that function also associates the given key with the computed
+ * default value in the map in case the map does not already contain the given key.
  *
  * Similar to the standard library's [getOrElse], but returns the value associated with the given [key] – rather than
  * calling the given [defaultProvider] and returning the result – also when that value is `null`.
@@ -41,6 +47,9 @@ inline fun <K, reified V> Map<K, V>.getOr(key: K, defaultProvider: () -> V): V =
  * Returns the value associated with the given [key] in the receiver map [this], or, if the key is not in the map, calls
  * the given [defaultProvider] with the key as the argument and returns the result.
  *
+ * Similar to [putIfAbsentAndGet] (for a mutable map), but that function also associates the given key with the computed
+ * default value in the map in case the map does not already contain the given key.
+ *
  * Similar to the standard library's [getOrElse], but returns the value associated with the given [key] – rather than
  * invoking the given [defaultProvider] and returning the result – also when that value is `null`. Also, this overload
  * takes a function to compute the default value which receives the key as an argument; there's another overload which
@@ -58,3 +67,76 @@ inline fun <K, reified V> Map<K, V>.getOr(key: K, defaultProvider: (K) -> V): V 
     val ret = get(key)
     return ret ?: if (ret is V && containsKey(key)) ret else defaultProvider(key)
 }
+
+/**
+ * Returns the value associated with the given [key] in the receiver mutable map [this], or, if the key is not in the
+ * map, associates the key with the given [default] and returns it.
+ *
+ * Similar to [getOr], but when the map does not contain the given key this function not only returns the given default,
+ * but also associates the given key with it in the map.
+ *
+ * Similar to the standard library's [getOrPut], but returns the value associated with the given [key] – rather than
+ * associating it with the given [default] and returning it – also when that value is `null`. Also, this overload takes
+ * the default value itself, rather than a function which computes it; there's another overload which takes such a
+ * function, as well as an overload which takes a function to provide the default which receives the key as an argument.
+ *
+ * This operation is atomic (meaning it relies on a single call to the receiver mutable map [this]'s [get] function)
+ * when the map contains the given [key] with an associated value which is not `null`. In all other cases (that is when
+ * the map does not contain the key, or, in case the type [V] is nullable, when it contains the key with an associated
+ * value of `null`), the operation is subject to race conditions when the map is changed concurrently, and may
+ * potentially exhibit behavior and/or results inconsistent with the map's state (such as returning a value the map
+ * never associated with the given key, or associating the given key with the given default value when the map already
+ * contains the key).
+ */
+inline fun <K, reified V> MutableMap<K, V>.putIfAbsentAndGet(key: K, default: V): V =
+    putIfAbsentAndGet<K, V>(key) { -> default }
+
+/**
+ * Returns the value associated with the given [key] in the receiver mutable map [this], or, if the key is not in the
+ * map, invokes the given [defaultProvider] and associates the key with the result and returns it.
+ *
+ * Similar to [getOr], but when the map does not contain the given key this function not only computes and returns a
+ * default, but also associates the given key with it in the map.
+ *
+ * Similar to the standard library's [getOrPut], but returns the value associated with the given [key] – rather than
+ * calling the given [defaultProvider] and associating the key with the result and returning it – also when that value
+ * is `null`.
+ *
+ * For passing the default value directly rather than a function which computes it, or to pass a function to compute the
+ * default value which receives the key as an argument, use one of the other overloads.
+ *
+ * This operation is atomic (meaning it relies on a single call to the receiver mutable map [this]'s [get] function)
+ * when the map contains the given [key] with an associated value which is not `null`. In all other cases (that is when
+ * the map does not contain the key, or, in case the type [V] is nullable, when it contains the key with an associated
+ * value of `null`), the operation is subject to race conditions when the map is changed concurrently, and may
+ * potentially exhibit behavior and/or results inconsistent with the map's state (such as returning a value the map
+ * never associated with the given key, or invoking the given [defaultProvider] and associating the key with the result
+ * when the map already contains the key).
+ */
+inline fun <K, reified V> MutableMap<K, V>.putIfAbsentAndGet(key: K, defaultProvider: () -> V): V =
+    putIfAbsentAndGet<K, V>(key) { _ -> defaultProvider() }
+
+/**
+ * Returns the value associated with the given [key] in the receiver mutable map [this], or, if the key is not in the
+ * map, calls the given [defaultProvider] with the key as the argument and associates the key with the result and
+ * returns it.
+ *
+ * Similar to [getOr], but when the map does not contain the given key this function not only computes and returns a
+ * default, but also associates the given key with it in the map.
+ *
+ * Similar to the standard library's [getOrPut], but returns the value associated with the given [key] – rather than
+ * calling the given [defaultProvider] and associating the key with the result and returning it – also when that value
+ * is `null`. Also, this overload takes a function to compute the default value which receives the key as an argument;
+ * there's another overload which takes a function with no parameters to compute the default, as well as an overload
+ * which takes the default value itself rather than a function.
+ *
+ * This operation is atomic (meaning it relies on a single call to the receiver mutable map [this]'s [get] function)
+ * when the map contains the given [key] with an associated value which is not `null`. In all other cases (that is when
+ * the map does not contain the key, or, in case the type [V] is nullable, when it contains the key with an associated
+ * value of `null`), the operation is subject to race conditions when the map is changed concurrently, and may
+ * potentially exhibit behavior and/or results inconsistent with the map's state (such as returning a value the map
+ * never associated with the given key, or invoking the given [defaultProvider] and associating the key with the result
+ * when the map already contains the key).
+ */
+inline fun <K, reified V> MutableMap<K, V>.putIfAbsentAndGet(key: K, defaultProvider: (K) -> V): V =
+    getOr(key) { -> defaultProvider(key).also { put(key, it) } }
--- src/commonTest/kotlin/MapsTest.kt
+++ src/commonTest/kotlin/MapsTest.kt
@@ -23,6 +23,10 @@ class MapsTest {
     private val nullableMap: Map<String, String?> get() = mapOf(Key to Value, KeyWithNullValue to null)
     private val oneCallNullableMap: Map<String, String?> get() = oneCallMap(Value)
 
+    private val nonNullableMutableMap: MutableMap<String, String> get() = mutableMapOf(Key to Value)
+    private val nullableMutableMap: MutableMap<String, String?> get() =
+        mutableMapOf(Key to Value, KeyWithNullValue to null)
+
     @Test fun inNonNullableMapGetOrGets() = testGetOr(Key, Value) { nonNullableMap }
     @Test fun inNonNullableMapGetOrOnlyRequiresOneCallToGet() = testGetOr(Key, Value) { oneCallNonNullableMap }
     @Test fun inNonNullableMapGetOrReturnsDefault() = testGetOr(NonExistingKey, Default) { nonNullableMap }
@@ -34,8 +38,48 @@ class MapsTest {
     @Test fun inNullableMapGetOrGetsNull() = testGetOr(KeyWithNullValue, null) { nullableMap }
     @Test fun inNullableMapGetOrReturnsDefault() = testGetOr(NonExistingKey, Default) { nullableMap }
 
+    @Test fun inNonNullableMutableMapPutIfAbsentAndGetGets() =
+        nonNullableMutableMap.testPutIfAbsentAndGetWithSideEffects(Key, Value)
+    @Test fun inNonNullableMutableMapPutIfAbsentAndGetOnlyRequiresOneCallToGet() =
+        testPutIfAbsentAndGet(Key, Value) { oneCallMutableMap<String>(Value) }
+    @Test fun inNonNullableMutableMapPutIfAbsentAndGetPutsAndReturnsDefault() =
+        nonNullableMutableMap.testPutIfAbsentAndGetWithSideEffects(NonExistingKey, Default)
+    @Test fun inNonNullableMutableMapPutIfAbsentAndGetOnlyRequiresOneGetToPutAndReturnDefault() =
+        testPutIfAbsentAndGet(Key, Default) { oneGetMutableMap<String>(null) }
+
+    @Test fun inNullableMutableMapPutIfAbsentAndGetGets() =
+        nullableMutableMap.testPutIfAbsentAndGetWithSideEffects(Key, Value)
+    @Test fun inNullableMutableMapPutIfAbsentAndGetOnlyRequiresOneCallToGetNonNull() =
+        testPutIfAbsentAndGet(Key, Value) { oneCallMutableMap<String?>(Value) }
+    @Test fun inNullableMutableMapPutIfAbsentAndGetGetsNull() =
+        nullableMutableMap.testPutIfAbsentAndGetWithSideEffects(KeyWithNullValue, null)
+    @Test fun inNullableMutableMapPutIfAbsentAndGetPutsAndReturnsDefault() =
+        nullableMutableMap.testPutIfAbsentAndGetWithSideEffects(NonExistingKey, Default)
+
     private inline fun <reified V> testGetOr(key: String, expectedValue: V, mapProvider: () -> Map<String, V>) =
         test(mapProvider, key, expectedValue, Map<String, V>::getOr, Map<String, V>::getOr, Map<String, V>::getOr)
+
+    private inline fun <reified V> testPutIfAbsentAndGet(
+        key: String, expectedValue: V, mapProvider: () -> MutableMap<String, V>
+    ) = test(
+        mapProvider, key, expectedValue,
+        MutableMap<String, V>::putIfAbsentAndGet,
+        MutableMap<String, V>::putIfAbsentAndGet,
+        MutableMap<String, V>::putIfAbsentAndGet
+    )
+
+    private inline fun <reified V> MutableMap<String, V>.testPutIfAbsentAndGetWithSideEffects(
+        key: String, expectedValue: V
+    ) {
+        val expectedResult = toMutableMap()
+        expectedResult[key] = expectedValue
+        test(
+            ::toMutableMap, key, expectedValue,
+            MutableMap<String, V>::putIfAbsentAndGet,
+            MutableMap<String, V>::putIfAbsentAndGet,
+            MutableMap<String, V>::putIfAbsentAndGet
+        ) { assertEquals(expectedResult, it) }
+    }
 
     private inline fun <M : Map<String, V>, reified V> test(
         mapProvider: () -> M,
@@ -43,27 +87,44 @@ class MapsTest {
         expectedValue: V,
         valueOverloadToTest: M.(String, V) -> V,
         providerOverloadToTest: M.(String, () -> V) -> V,
-        functionOverloadToTest: M.(String, (String) -> V) -> V
+        functionOverloadToTest: M.(String, (String) -> V) -> V,
+        extraVerification: (M) -> Unit = {}
     ) {
         val default = Default as V
         val isDefault = expectedValue == default
         val provider = if (isDefault) scriptedProvider(default) else mootProvider
         val function = if (isDefault) scriptedFunction(key to default) else mootFunction
 
-        assertEquals(expectedValue, mapProvider().valueOverloadToTest(key, default))
-        assertEquals(expectedValue, mapProvider().providerOverloadToTest(key, provider))
-        assertEquals(expectedValue, mapProvider().functionOverloadToTest(key, function))
+        val maps = List(3) { mapProvider() }
+        assertEquals(expectedValue, maps[0].valueOverloadToTest(key, default))
+        assertEquals(expectedValue, maps[1].providerOverloadToTest(key, provider))
+        assertEquals(expectedValue, maps[2].functionOverloadToTest(key, function))
+
+        maps.forEach(extraVerification)
     }
 
-    private fun <V> oneCallMap(valueOrNull: V?): Map<String, V> = object : Map<String, V> {
+    private fun <V> oneCallMap(valueOrNull: V?): Map<String, V> = oneCallMutableMap(valueOrNull)
+    private fun <V> oneCallMutableMap(valueOrNull: V?): MutableMap<String, V> =
+        oneGetMutableMap(valueOrNull) { key, value -> mootFunction(key to value) }
+    private fun <V> oneGetMutableMap(
+        valueOrNull: V?,
+        put: (String, V) -> Unit = { _, _ -> }
+    ): MutableMap<String, V> = object : MutableMap<String, V> {
         private val getFunction: (String) -> V? = scriptedFunction(Key to valueOrNull)
         override fun get(key: String): V? = getFunction(key)
+        override fun put(key: String, value: V): V? {
+            put(key, value)
+            return null
+        }
 
-        override val entries: Set<Map.Entry<String, V>> get() = mootProvider()
-        override val keys: Set<String> get() = mootProvider()
+        override val entries: MutableSet<MutableMap.MutableEntry<String, V>> get() = mootProvider()
+        override val keys: MutableSet<String> get() = mootProvider()
         override val size: Int get() = mootProvider()
-        override val values: Collection<V> get() = mootProvider()
+        override val values: MutableCollection<V> get() = mootProvider()
+        override fun clear() = mootProvider()
         override fun isEmpty(): Boolean = mootProvider()
+        override fun remove(key: String): V? = mootFunction(key)
+        override fun putAll(from: Map<out String, V>) = mootFunction(from)
         override fun containsValue(value: V): Boolean = mootFunction(value)
         override fun containsKey(key: String): Boolean = mootFunction(key)
     }
