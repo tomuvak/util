36dfc80934e4caed6a441eb447474ee9a1742f5e
Tom Yuval <tomuvak@gmail.com>
2022-08-14 12:51:21 +0200

    Lists: add overload List<>.replaceAt(vararg indexToNewValue)

--- src/commonMain/kotlin/Lists.kt
+++ src/commonMain/kotlin/Lists.kt
@@ -3,8 +3,28 @@ package com.tomuvak.util
 /**
  * Returns a list with the same size and elements as the receiver list [this], except that the element at the given
  * [index] (which has to be a valid index for the list) is the given [newValue].
+ *
+ * For replacing the elements at multiple indices, the other overload might be more efficient than multiple invocations
+ * of this one.
  */
 fun <T> List<T>.replaceAt(index: Int, newValue: T): List<T> {
     if (index !in indices) throw IndexOutOfBoundsException("Index $index out of bounds (size: $size)")
     return mapIndexed { i, oldValue -> if (i == index) newValue else oldValue }
+}
+
+/**
+ * Returns a list with the same size and elements as the receiver list [this], except that for any pair of index and new
+ * value in the given [indexToNewValue], the element at the index given is the new value given. Each of the indices has
+ * to be a valid index for the list, and the indices must be distinct.
+ *
+ * For replacing the element at a single index, the other overload might be more efficient than this one.
+ */
+inline fun <reified T> List<T>.replaceAt(vararg indexToNewValue: Pair<Int, T>): List<T> {
+    val replacements = mutableMapOf<Int, T>()
+    for ((index, newValue) in indexToNewValue) {
+        if (index !in indices) throw IndexOutOfBoundsException("Index $index out of bounds (size: $size)")
+        if (index in replacements) throw IllegalArgumentException("Repeating index: $index")
+        replacements[index] = newValue
+    }
+    return mapIndexed { index, oldValue -> replacements.getOr(index, oldValue) }
 }
--- src/commonTest/kotlin/ListsTest.kt
+++ src/commonTest/kotlin/ListsTest.kt
@@ -14,14 +14,42 @@ class ListsTest {
                         list.replaceAt(index, newValue)
                     )
     }
-
     @Test fun replaceAtThrowsWhenIndexOutOfBounds() {
         for (list in listOf(emptyList(), listOf(1), listOf(2, 2), listOf(1, 3, 3, 7)))
             for (index in -3..list.size + 2)
                 if (index !in list.indices)
                     for (newValue in listOf(0, 1, 5))
-                        assertFailsWithTypeAndMessageContaining<IndexOutOfBoundsException>(index, list.size) {
-                            list.replaceAt(index, newValue)
-                        }
+                        thenFails (index, list.size) { list.replaceAt(index, newValue) }
     }
+
+    @Test fun replaceAtZero() {
+        for (list in listOf(listOf(1), listOf(2, 2), listOf(1, 3, 3, 7)))
+            assertEquals(list, list.replaceAt())
+    }
+    @Test fun replaceAtOne() {
+        for (list in listOf(listOf(1), listOf(2, 2), listOf(1, 3, 3, 7)))
+            for (index in list.indices)
+                for (newValue in listOf(0, 1, 5))
+                    assertEquals(list.replaceAt(index, newValue), list.replaceAt(index to newValue))
+    }
+    @Test fun replaceAtMultiple() {
+        assertEquals(listOf(3, 4), listOf(1, 2).replaceAt(0 to 3, 1 to 4))
+        assertEquals(listOf(7, 0, 8), listOf(0, 0, 1).replaceAt(2 to 8, 0 to 7))
+        assertEquals(listOf(5, 0, 1, 8, 2), listOf(5, 6, 7, 8, 9).replaceAt(1 to 0, 4 to 2, 2 to 1))
+        assertEquals(listOf(null, 1, 2), listOf(0, 1, null).replaceAt(0 to null, 2 to 2))
+    }
+    @Test fun replaceAtMultipleThrowsWhenIndexOutOfBounds() {
+        thenFails(0, 0) { emptyList<Int>().replaceAt(0 to 3) }
+        thenFails(-1, 1) { listOf(1).replaceAt(-1 to 4)}
+        thenFails(4, 3) { listOf(0, 1, 2).replaceAt(0 to 5, 2 to 6, 4 to 7)}
+    }
+    @Test fun replaceAtMultipleThrowsWhenRepeatingIndex() {
+        thenFails(0) { listOf(0).replaceAt(0 to 1, 0 to 1) }
+        thenFails(1) { listOf(1, 2, 3).replaceAt(1 to 4, 2 to 4, 1 to 4)}
+    }
+
+    private fun thenFails(index: Int, size: Int, block: () -> Unit) =
+        assertFailsWithTypeAndMessageContaining<IndexOutOfBoundsException>(index, size, block=block)
+    private fun thenFails(index: Int, block: () -> Unit) =
+        assertFailsWithTypeAndMessageContaining<IllegalArgumentException>("epeating index", index, block=block)
 }
